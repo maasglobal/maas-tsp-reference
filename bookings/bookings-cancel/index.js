8b83eb7cea1e9f110376d6cbb6ffed11a83f7083
Lauri Svan <lauri.svan@iki.fi>
2016-08-12 12:27:06 +0300

    Fix according to pull request comments

--- bookings/bookings-cancel/index.js
+++ bookings/bookings-cancel/index.js
@@ -8,43 +8,28 @@ function validateRequest(event) {
   return Promise.resolve(true);
 }
 
-function verifyResponse(response) {
-  if (response.Error !== null) {
-    return Promise.reject(new Error('400: Bad Request: ' + JSON.stringify(response.Error, null, 2)));
-  }
-
-  return Promise.resolve(response.Result);
-}
-
-function format(status) {
-  if ( ! status.Cancellation.Ok ) {
-    return Promise.reject(new Error('500: Cancellation was not Ok even though there were no errors'));
-  }
-
-  const result = {
-    state: 'CANCELLED',
-  };
-  if ( status.Cancellation.Fee ) {
-    result.meta = {
-      cancellationFee: {
-        amount: status.Cancellation.Fee.DueAmount,
-        currency: status.Cancellation.Fee.Currency,
-      },
-    };
-  }
-  return result;
-}
-
-module.exports.respond = function (event, cb) {
+function handleResponse(callback) {
   const newData = {
     state: 'CANCELLED',
     meta: {
       string: 'Retain this string accross requests',
       object: {
         key: 'value',
-      }
-    }
+      },
+    },
   };
 
-  cb(null, newData);
-};
+  callback(null, newData);
+}
+
+function handleError(error, callback) {
+  console.warn('An error occurred', error.message);
+  console.warn('Stack:', error.stack);
+
+  callback(error);
+}
+
+module.exports.respond = function (event, cb) {
+  validateRequest(event)
+    .then(() => handleResponse(cb), error => handleError(error, cb));
+}
