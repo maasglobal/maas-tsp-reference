d6f19229e9af0ea01af09a4110d3e197f6602cbf
Tomi Niittumäki <tomi.niittumaki@gmail.com>
2016-08-29 16:24:34 +0300

    Add sls offline server for mocha tests

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,14 @@ const eslint = require('gulp-eslint');
 const gmocha = require('gulp-mocha');
 const gulpSequence = require('gulp-sequence');
 const gutil = require('gulp-util');
+const spawn = require('child_process').spawn;
 
 const jsoncFiles = ['.eslintrc']; // json with comments
 const jsonFiles = ['**/*.json', '!**/node_modules/**/*.json', '!_meta/**/*.json', '!schemas/**/*.json'];
 const jsFiles = ['**/*.js', '!**/node_modules/**/*.js', '!_meta/**/*.js', '!schemas/**/*.js'];
+
+// Run sls offline in background while performing mocha tests
+let localServer;
 
 gulp.task('jsonclint', () => {
 
@@ -37,20 +41,34 @@ gulp.task('eslint', () => {
     .pipe(eslint.failAfterError());
 });
 
+// Local test server config
+gulp.task('serverStart', cb => {
+  localServer = spawn('sls', ['offline', 'start']);
+  localServer.stdout.on('data', chunk => {
+    if (chunk.toString().indexOf('listening on') > -1) {
+      console.log(chunk.toString());
+      cb();
+    }
+  });
+});
+
+gulp.task('serverStop', () => {
+  localServer.kill('SIGTERM');
+  console.log('Stopped Serverless Offline!');
+  process.exit();
+});
+
 gulp.task('mocha', () => {
   return gulp.src('tests/test.js', { read: false })
     .pipe(gmocha({ grep: argv.grep }))
     .on('error', error => {
       gutil.log(error);
       process.exit(1);
-    })
-    .on('end', () => {
-      process.exit();
     });
 });
 
 gulp.task('validate', ['jsonclint', 'jsonlint', 'eslint']);
 
-gulp.task('test', gulpSequence('validate', 'mocha'));
+gulp.task('test', ['serverStart'], gulpSequence('validate', 'mocha', 'serverStop'));
 
 gulp.task('default');
